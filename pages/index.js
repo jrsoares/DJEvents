ee5a0a449c48e7d88988b0e4b74202b4bd069523
Jr.Soares <juniorsoaresti@gmail.com>
2021-07-02 14:11:20 -0400

    Display Events

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,32 @@
 import Layout from "@/components/Layout";
+import Link from "next/link";
+import { API_URL } from "@/config/index";
+import EventItem from "@/components/EventItem";
 
-export default function Home() {
+export default function HomePage({ events }) {
+  console.log(events);
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1>Upcoming Events</h1>
+      {events.lenght === 0 && <h3>No events show</h3>}
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
+      {events.length > 0 && (
+        <Link href="/events">
+          <a className="btn-secondary">View all</a>
+        </Link>
+      )}
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  return {
+    props: { events: events.slice(0, 3) },
+    revalidate: 1,
+  };
 }
