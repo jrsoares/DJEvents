9720bd164ae0d8a127bb98ebbe460db0987fb897
Jr.Soares <juniorsoaresti@gmail.com>
2021-07-05 20:26:02 -0400

    getStaticPaths

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,12 +1,42 @@
 import { useRouter } from "next/router";
 import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
 
-export default function EventPage() {
+export default function EventPage({ evt }) {
   const router = useRouter();
   return (
     <Layout>
       <h1>My event</h1>
-      <h3>{router.query.slug}</h3>
+      <h3>{evt.name}</h3>
     </Layout>
   );
 }
+
+export async function getStaticPaths() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+  const paths = events.map((evt) => ({ params: { slug: evt.slug } }));
+  return {
+    paths,
+    fallback: true,
+  };
+}
+
+export async function getStaticProps({ params: { slug } }) {
+  const res = await fetch(`${API_URL}/api/events/${slug}`);
+  const events = await res.json();
+  return {
+    props: {
+      evt: events[0],
+    },
+    revalidate: 1,
+  };
+}
+
+// export async function getServerSideProps({ query: { slug } }) {
+//   const res = await fetch(`${API_URL}/api/events/${slug}`);
+//   const events = await res.json();
+//   return {
+//     props: { evt: events[0] },
+//   };
+// }
