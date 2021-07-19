9f289cc363964dd95ab2d4d364526621475bd465
Jr.Soares <juniorsoaresti@gmail.com>
2021-07-19 09:54:22 -0400

    Add event page

--- components/Header.js
+++ components/Header.js
@@ -17,6 +17,11 @@ export default function Header() {
               <a>Events</a>
             </Link>
           </li>
+          <li>
+            <Link href="/events/add">
+              <a>Add</a>
+            </Link>
+          </li>
         </ul>
       </nav>
     </header>
