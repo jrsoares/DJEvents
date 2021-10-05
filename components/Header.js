e9da5d418c0922395ce8c261cd4e5637b9b85364
Jr.Soares <juniorsoaresti@gmail.com>
2021-10-05 13:41:54 -0400

    Login Forms and Auth Context

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,15 @@
+import { useContext } from 'react';
+import AuthContext from '@/context/AuthContext'
+import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa'
 import Link from "next/link";
 import Search from "@/components/Search";
 import styles from "@/styles/Header.module.css";
+
+
 export default function Header() {
+
+  const { user, logout } = useContext(AuthContext);
+
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
@@ -17,11 +25,33 @@ export default function Header() {
               <a>Events</a>
             </Link>
           </li>
-          <li>
-            <Link href="/events/add">
-              <a>Add</a>
-            </Link>
-          </li>
+          {user ?
+            // if logged in
+            <>
+              <li>
+                <Link href="/events/add">
+                  <a>Add Event</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/account/dashboard">
+                  <a>Dasboard</a>
+                </Link>
+              </li>
+              <li>
+                <button className="btn-secondary btn-icon" onClick={() => logout()}>
+                  <FaSignOutAlt /> Logout
+                </button>
+              </li>
+            </> :
+            // if loged out
+            <>
+              <li>
+                <Link href="/account/login">
+                  <a className='btn-secondary btn-icon'><FaSignInAlt />Login</a>
+                </Link>
+              </li>
+            </>}
         </ul>
       </nav>
     </header>
